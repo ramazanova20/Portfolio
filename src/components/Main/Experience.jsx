c2e09d4726f9d36d504377f2330ef9c3a00a50b3
gunay.2000ramazanova@gmail.com <you@example.com>
2025-02-26 00:45:50 +0400

    Updating Design2

--- src/components/Main/Experience.jsx
+++ src/components/Main/Experience.jsx
@@ -10,7 +10,7 @@ function Experience() {
             <div>
                 <div className='py-4'>
                     <div className="flex flex-col lg:flex-row justify-between py-6">
-                    <section className="text-white py-12 lg:py-20 tilt-in-left-2 border-l-8 border-t-8 border-solid border-[#139504] p-4">
+                    <section className="text-white max-w-[100%] overflow-x-hidden py-12 lg:py-20 tilt-in-left-2 border-l-8 border-t-8 border-solid border-[#139504] p-4">
                         <h2 className="text-3xl font-bold mb-4 text-center lg:text-left hover:text-yellow-400">Frontend Developer Intern</h2>
                         <ul className="lg:list-disc pl-6 space-y-2 text-lg/7 text-center lg:text-left">
                             <li>Designed UI/UX for application and web pages using Figma.</li> 
@@ -20,7 +20,7 @@ function Experience() {
                             <li><strong>Tech Stack:</strong> Figma, HTML, CSS, JavaScript</li>
                         </ul>
                     </section>
-                    <a href='https://migration.gov.az/en' target="_blank" className="lg:w-[50%] tilt-in-right-2 border-r-8 border-b-8 border-solid border-[#139504] p-4">
+                    <a href='https://migration.gov.az/en' target="_blank" className="lg:w-[50%] max-w-[100%] overflow-x-hidden tilt-in-right-2 border-r-8 border-b-8 border-solid border-[#139504] p-4">
                         <img src={migration} alt="MigrationService" className='py-12 w-full'/>
                         {/* <img src="https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" /> */}
                     </a>
@@ -28,7 +28,7 @@ function Experience() {
                 </div>
                 <div className='py-4'>
                     <div className="flex flex-col lg:flex-row-reverse justify-between py-6">
-                    <section className="text-white py-12 lg:py-20 tilt-in-left-2 border-l-8 border-t-8 lg:border-r-8 lg:border-b-8 lg:border-l-0 lg:border-t-0 border-solid border-[#139504] p-4">
+                    <section className="text-white py-12 max-w-[100%] overflow-x-hidden lg:py-20 tilt-in-left-2 border-l-8 border-t-8 lg:border-r-8 lg:border-b-8 lg:border-l-0 lg:border-t-0 border-solid border-[#139504] p-4">
                         <h2 className="text-3xl font-bold mb-4 text-center lg:text-left hover:text-yellow-400">Frontend Developer Intern</h2>
                         <ul className="lg:list-disc pl-6 space-y-2 text-lg/7 text-center lg:text-left">
                             <li>Built a movie website by integrating TVMaze and Custom APIs for real-time movie and TV show data.</li> 
@@ -38,7 +38,7 @@ function Experience() {
                             <li><strong>Tech Stack:</strong> React.js, Tailwind CSS, TVMaze API, Custom API</li>
                         </ul>
                     </section>
-                    <a href='https://www.linkedin.com/company/intern-intelligence/posts/?feedView=all' target="_blank" className="flex p-4 border-r-8 border-b-8 lg:border-l-8 lg:border-t-8 border-solid lg:border-r-0 lg:border-b-0 border-[#139504] lg:w-[50%] tilt-in-right-2">
+                    <a href='https://www.linkedin.com/company/intern-intelligence/posts/?feedView=all' target="_blank" className="flex p-4 border-r-8 border-b-8 lg:border-l-8 lg:border-t-8 border-solid lg:border-r-0 lg:border-b-0 border-[#139504] max-w-[100%] overflow-x-hidden lg:w-[50%] tilt-in-right-2">
                         <img src={intern} alt="InternIntelligence" className='py-12 w-full'/>
                         {/* <img src="https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" /> */}
                     </a>
--- src/components/Main/Projects.jsx
+++ src/components/Main/Projects.jsx
@@ -15,7 +15,7 @@ function Projects() {
            </div>
             <div className='py-4'>
                 <div className="flex flex-col lg:flex-row justify-between py-6">
-                    <section className="text-white py-12 lg:py-36 tilt-in-left-2 border-l-8 border-t-8 border-solid border-[#139504] p-4">
+                    <section className="text-white py-12 max-w-[100%] overflow-x-hidden lg:py-36 tilt-in-left-2 border-l-8 border-t-8 border-solid border-[#139504] p-4">
                         <h2 className="text-3xl font-bold mb-4 text-center lg:text-left hover:text-yellow-400">Project Highlights</h2>
                         <ul className="lg:list-disc pl-6 space-y-2 text-lg/7 text-center lg:text-left">
                             <li>A movie and TV series platform that fetches data from TVMaze and Custom APIs.</li> 
@@ -25,12 +25,12 @@ function Projects() {
                             <li><strong>Tech Stack:</strong> React.js, Tailwind CSS, React Router, TVMaze API, Custom API</li>
                         </ul>
                     </section>
-                    <a href='https://movie-world-bay.vercel.app/' target="_blank" className="border-r-8 border-b-8 border-solid border-[#139504] p-4 lg:w-[50%] tilt-in-right-2">
+                    <a href='https://movie-world-bay.vercel.app/' target="_blank" className="border-r-8 border-b-8 border-solid border-[#139504] p-4 max-w-[100%] overflow-x-hidden lg:w-[50%] tilt-in-right-2">
                         <img src={movie} alt="Movie" className='lg:py-12 w-full'/>
                     </a>
                 </div>
                 <div className="flex flex-col lg:flex-row-reverse justify-between py-6">
-                    <section className="text-white py-12 lg:py-20 tilt-in-left-2 border-l-8 border-t-8 lg:border-r-8 lg:border-b-8 lg:border-l-0 lg:border-t-0 border-solid border-[#139504] p-4">
+                    <section className="text-white py-12 max-w-[100%] overflow-x-hidden lg:py-20 tilt-in-left-2 border-l-8 border-t-8 lg:border-r-8 lg:border-b-8 lg:border-l-0 lg:border-t-0 border-solid border-[#139504] p-4">
                         <h2 className="text-3xl font-bold mb-4 text-center lg:text-left hover:text-yellow-400">Project Highlights</h2>
                         <ul className="lg:list-disc pl-6 space-y-2 text-center text-lg/7 lg:text-left">
                             <li>An online beauty store showcasing various cosmetic products.</li> 
@@ -41,12 +41,12 @@ function Projects() {
                             <li><strong>Tech Stack:</strong> React.js, Tailwind CSS, React Router, Makeup API, Custom API</li>
                         </ul>
                     </section>
-                    <a href='https://cosmetics-site-bay.vercel.app/' target="_blank" className="flex p-4 border-r-8 border-b-8 lg:border-l-8 lg:border-t-8 border-solid lg:border-r-0 lg:border-b-0 border-[#139504] lg:w-[50%] tilt-in-right-2">
+                    <a href='https://cosmetics-site-bay.vercel.app/' target="_blank" className="flex p-4 max-w-[100%] overflow-x-hidden border-r-8 border-b-8 lg:border-l-8 lg:border-t-8 border-solid lg:border-r-0 lg:border-b-0 border-[#139504] lg:w-[50%] tilt-in-right-2">
                         <img src={cosmetics} alt="Cosmetics" className='py-12 w-full'/>
                     </a>
                 </div>
                 <div className="flex flex-col lg:flex-row justify-between py-6">
-                    <section className="text-white py-12 lg:py-20 tilt-in-left-2 border-l-8 border-t-8 border-solid border-[#139504] p-4">
+                    <section className="text-white py-12 max-w-[100%] overflow-x-hidden lg:py-20 tilt-in-left-2 border-l-8 border-t-8 border-solid border-[#139504] p-4">
                         <h2 className="text-3xl font-bold mb-4 text-center lg:text-left hover:text-yellow-400">Project Highlights</h2>
                         <ul className="lg:list-disc pl-6 space-y-2 text-lg/7 text-center lg:text-left">
                             <li>A sleek and minimalist landing page inspired by Apple’s design.</li> 
@@ -56,12 +56,12 @@ function Projects() {
                             <li><strong>Tech Stack:</strong> HTML, CSS, Bootstrap</li>
                         </ul>
                     </section>
-                    <a href='https://apple-green-two.vercel.app/' target="_blank" className="lg:w-[50%] tilt-in-right-2 border-r-8 border-b-8 border-solid border-[#139504] p-4">
+                    <a href='https://apple-green-two.vercel.app/' target="_blank" className="max-w-[100%] overflow-x-hidden lg:w-[50%] tilt-in-right-2 border-r-8 border-b-8 border-solid border-[#139504] p-4">
                         <img src={apple} alt="Apple" className=' py-12 w-full'/>
                     </a>
                 </div>
                 <div className="flex flex-col lg:flex-row-reverse justify-between py-6">
-                    <section className="text-white py-12 lg:py-20 tilt-in-left-2 border-l-8 border-t-8 lg:border-r-8 lg:border-b-8 lg:border-l-0 lg:border-t-0 border-solid border-[#139504] p-4">
+                    <section className="text-white py-12 max-w-[100%] overflow-x-hidden lg:py-20 tilt-in-left-2 border-l-8 border-t-8 lg:border-r-8 lg:border-b-8 lg:border-l-0 lg:border-t-0 border-solid border-[#139504] p-4">
                         <h2 className="text-3xl font-bold mb-4 text-center lg:text-left hover:text-yellow-400">Project Highlights</h2>
                         <ul className="lg:list-disc pl-6 space-y-2 text-lg/7 text-center lg:text-left">
                             <li>A simple and professional website for medical institutions.</li> 
@@ -71,7 +71,7 @@ function Projects() {
                             <li><strong>Tech Stack:</strong> HTML, CSS, Bootstrap</li>
                         </ul>
                     </section>
-                    <a href='https://medical-nine-beta.vercel.app/' target="_blank" className="p-4 border-r-8 border-b-8 lg:border-l-8 lg:border-t-8 border-solid lg:border-r-0 lg:border-b-0 border-[#139504] lg:w-[50%] tilt-in-right-2">
+                    <a href='https://medical-nine-beta.vercel.app/' target="_blank" className="max-w-[100%] overflow-x-hidden p-4 border-r-8 border-b-8 lg:border-l-8 lg:border-t-8 border-solid lg:border-r-0 lg:border-b-0 border-[#139504] lg:w-[50%] tilt-in-right-2">
                         <img src={medical} alt="Medical" className='py-12 w-full'/>
                     </a>
                 </div>
